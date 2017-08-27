602d1a0bcb748e2b6c188f51238b994ea30c2ebe
Jiawei Guo <acthse@outlook.com>
2017-08-27 20:37:27 +0800

    Update error.py

--- orm/error.py
+++ orm/error.py
@@ -3,10 +3,10 @@
 
 
 class DeleteNoneTypeError(Exception):
-	"""删除为保存对象"""
+	"""删除未保存对象"""
 	
 class NoDbConnPathError(Exception):
-	""" 没有为任务配置数据库连接"""
+	""" 没有为任务配置数据库连接 """
 
 class ArgTypeError(Exception):
 	""" 参数类型错误 """
